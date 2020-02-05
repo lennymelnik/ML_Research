1ff257002d724d7fef57021c73ebf823ee3ab6f1
Leonard Melnik <leonard@onedr.io>
2020-02-05 11:21:28 -0500

    It works. Makes 7x profit

--- Modular/__init__.py
+++ Modular/__init__.py
@@ -5,13 +5,13 @@ import requests
 import random
 import sys
 import csv
-
+import math
 
 
 with open("stocks.csv") as f:
     reader = csv.reader(f)
     liste = list(reader)
-print(liste[0])
+
 
 csv_list = []
 number2 = 0
@@ -41,105 +41,154 @@ df["Volume USD"] = new[7]
 
 
 seeResults = 'n'
-def convertToBTC(usdAmount, day):
+def convertToBTC(usdAmount, day, timerange):
 
-    return(usdAmount/int(float(df["Close"][timeRange - day])))
+    return(usdAmount/int(float(df["Close"][timerange - day])))
 
-def convertToUSD(btcAmount, day):
+def convertToUSD(btcAmount, day, timerange):
 
-    return(btcAmount *int(float(df["Close"][timeRange - day])))
+    return(btcAmount *int(float(df["Close"][timerange - day])))
 
 def SimpleMovingAvg(number, starting, sma = 0):
     for i in range(number):
         sma = sma + int(float(df["Close"][starting + 1 + i]))
     return sma/number
 
-def returnRisk(timerange, differentList, finalFund):
+def returnRisk(timerange, differenceList, finalFund):
     fivePercent = sorted(differenceList)
-    fivePercent = fivePercent[:round(timeRange *.05)]
+    fivePercent = fivePercent[:round(timerange *.05)]
     if(seeResults == 'y'):
-        print("We have 95% confidence that our loss will not exceed ", fivePercent[round(timeRange * .05 -1)]* .01 * finalFund, "which is ", fivePercent[round(timeRange * .05 -1)], "% of our funds" )
+        print("We have 95% confidence that our loss will not exceed ", fivePercent[round(timerange * .05 -1)]* .01 * finalFund, "which is ", fivePercent[round(timerange * .05 -1)], "% of our funds" )
         print("Funds", finalFund)
-    return(fivePercent[round(timeRange * .05 -1)])
+    return(fivePercent[round(timerange * .05 -1)])
 
-
-totalFunds = []
-differenceList = []
-
-randIntTop = []
-randIntBottom = []
-endValue = []
-buySell = []
-finalFund = 0
-timeRange = 365*4
-
-testingAmount = 100
-sys.stdout.write("[%s]" % (" " * 40))
-sys.stdout.flush()
-sys.stdout.write("\b" * (40+1))
-for i in range(testingAmount):
-
+def plot(sell, buy, price):
+    totalFunds = []
+    differenceList = []
     inUSD = 1000
-    startingFund = 1000
     inBitcoin = 0
-    valueBought = 0
+    timeRange = 365*4
 
-    if((i % round(testingAmount/40)) == 0):
-        sys.stdout.write("-")
-        sys.stdout.flush()
             
-    if(seeResults == 'y'):
-        print("First Day:   ", "Funds : ", inUSD, "   In Bitcoin: ", inBitcoin, "  Bitcoin Price: ", int(float(df["Close"][timeRange])), "     SMA:   ", SimpleMovingAvg(200,(timeRange )))
-    topTemp = random.randint(-1500,700)
-    top = topTemp * .01
-    bottom = random.randint(topTemp, 700) * .01
-    for i in range(100):
-        payPrice = .01 * i
-        for i in range(timeRange):
+       
+    print("First Day:   ", "Funds : ", inUSD, "   In Bitcoin: ", inBitcoin, "  Bitcoin Price: ", int(float(df["Close"][timeRange])), "     SMA:   ", SimpleMovingAvg(200,(timeRange )))
+
+    for i in range(timeRange - 1):
             
+            
+        print("here")
+        dayCompare = int(float(df["Close"][timeRange - i]))
+        dayPrevious = int(float(df["Close"][timeRange - i - 1]))
+        difference = (dayCompare / dayPrevious) * 100 - 100
+        differenceList.append(difference)
+                
+        if(i > round(timeRange *.1)):
+            finalFund = inUSD +convertToUSD(inBitcoin, i, timeRange)
+            percentLoss = returnRisk(timeRange, differenceList, finalFund)
                     
-            if (i < timeRange):
-
-                dayCompare = int(float(df["Close"][timeRange - i]))
-                dayPrevious = int(float(df["Close"][timeRange - i - 1]))
-                difference = (dayCompare / dayPrevious) * 100 - 100
-                differenceList.append(difference)
-                
-                if(i > round(timeRange *.1)):
-                    finalFund = inUSD +convertToUSD(inBitcoin, i)
-                    percentLoss = returnRisk(timeRange, differenceList, finalFund)
+            #if the percent loss is less than the tolerated amount then sell
+            if(percentLoss < sell):
                     
-
-                    if(percentLoss < top):
-                        if(seeResults == 'y'):
-                            print("sell")
-                        inUSD = inUSD + convertToUSD(inBitcoin * payPrice, i)
-                        inBitcoin = inBitcoin * (1 - payPrice)
-                    elif(percentLoss > bottom):
-                        if(seeResults == 'y'):
-                            print("buy/stay")
-                        inBitcoin = inBitcoin + convertToBTC(inUSD * payPrice, i)
-                        inUSD = inUSD * (1 - payPrice)
+                print("sell")
+                inUSD = inUSD + convertToUSD(inBitcoin * price, i, timeRange)
+                inBitcoin = inBitcoin * (1 - price)
+            elif(percentLoss > buy):
+                    
+                print("buy/stay")
+                inBitcoin = inBitcoin + convertToBTC(inUSD * price, i, timeRange)
+                inUSD = inUSD * (1 - price)
                 
-                totalFunds.append(inUSD + convertToUSD(inBitcoin, i))
-                
-                
-
-            else:
-                print("Done")
+        totalFunds.append(inUSD + convertToUSD(inBitcoin, i, timeRange))
+        print(inUSD + convertToUSD(inBitcoin, i, timeRange))
         
-        #plt.plot(np.linspace(0, len(totalFunds), len(totalFunds)), totalFunds)
-        #plt.show()
-        endValue.append(inUSD + convertToUSD(inBitcoin, i))
-        randIntTop.append(top)
-        randIntBottom.append(bottom)
-        buySell.append(payPrice)
-        totalFunds = []
-        differenceList = []
-        ##print(inUSD + convertToUSD(inBitcoin, i))
-    
-sys.stdout.write("]\n")
-print(max(endValue))
+    plt.plot(np.linspace(0, len(totalFunds), len(totalFunds)), totalFunds)
+    plt.show()
+def find():        
+    totalFunds = []
+    differenceList = []
 
-finalValue = endValue.index(max(endValue))
-print("Sell at:", randIntTop[finalValue], " Buy at: ", randIntBottom[finalValue], "price :", buySell[finalValue])
+    randIntTop = []
+    randIntBottom = []
+    endValue = []
+    buySell = []
+    finalFund = 0
+    timeRange = 365*3
+
+    testingAmount = 10
+    sys.stdout.write("[%s]" % (" " * 40))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (40+1))
+
+
+    for i in range(testingAmount):
+
+        inUSD = 1000
+  
+        inBitcoin = 0
+      
+
+        if((i % math.ceil(testingAmount/40)) == 0):
+            sys.stdout.write("-")
+            sys.stdout.flush()
+                
+        if(seeResults == 'y'):
+            print("First Day:   ", "Funds : ", inUSD, "   In Bitcoin: ", inBitcoin, "  Bitcoin Price: ", int(float(df["Close"][timeRange])), "     SMA:   ", SimpleMovingAvg(200,(timeRange )))
+        topTemp = random.randint(-1500,700)
+        top = topTemp * .01
+        bottom = random.randint(topTemp, 700) * .01
+        for i in range(100):
+            payPrice = .01 * i
+            for i in range(timeRange):
+                
+                        #for every day until the current
+                if (i < timeRange):
+
+                    dayCompare = int(float(df["Close"][timeRange - i]))
+                    dayPrevious = int(float(df["Close"][timeRange - i - 1]))
+                    difference = (dayCompare / dayPrevious) * 100 - 100
+                    differenceList.append(difference)
+                    
+                    if(i > round(timeRange *.1)):
+                        finalFund = inUSD +convertToUSD(inBitcoin, i, timeRange)
+                        percentLoss = returnRisk(timeRange, differenceList, finalFund)
+                        
+                        #if the percent loss is less than the tolerated amount then sell
+                        if(percentLoss < top):
+                            if(seeResults == 'y'):
+                                print("sell")
+                            inUSD = inUSD + convertToUSD(inBitcoin * payPrice, i, timeRange)
+                            inBitcoin = inBitcoin * (1 - payPrice)
+                        elif(percentLoss > bottom):
+                            if(seeResults == 'y'):
+                                print("buy/stay")
+                            inBitcoin = inBitcoin + convertToBTC(inUSD * payPrice, i, timeRange)
+                            inUSD = inUSD * (1 - payPrice)
+                    totalFunds = (inUSD + convertToUSD(inBitcoin, i, timeRange))
+                  
+                    
+                    
+
+                else:
+                    print("Done")
+            
+            #plt.plot(np.linspace(0, len(totalFunds), len(totalFunds)), totalFunds)
+            #plt.show()
+            endValue.append(totalFunds)
+            randIntTop.append(top)
+            randIntBottom.append(bottom)
+            buySell.append(payPrice)
+            
+            differenceList = []
+            totalFunds = 0
+            ##print(inUSD + convertToUSD(inBitcoin, i))
+
+
+        
+        
+    sys.stdout.write("]\n")
+    print(max(endValue))
+
+    finalValue = endValue.index(max(endValue))
+    print("Sell at:", randIntTop[finalValue], " Buy at: ", randIntBottom[finalValue], "price :", buySell[finalValue])
+plot(-4.19,-1.6500000000000001,0.09)
+#find()
